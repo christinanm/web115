22def21a4509e5b5b6377b055383a633c60e4afe
Christina Mekheil <mekheilc@gmail.com>
2024-09-10 17:35:09 -0400

    changed jsfiddle link and fizz buzz beginnings

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function fizzBuzz(){
+    alert("Submitted successfuly!");
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var middleInitial = document.getElementById("middleinitial").value;
+
+    document.getElementById("greeting").innerText = "Welcome to Cy\'s Computers, " + firstName + " " + middleInitial + " " + lastName + "!";
+}
