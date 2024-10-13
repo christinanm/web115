d313c292a3942becf548269989e3c48c31ffcb39
Christina Mekheil <mekheilc@gmail.com>
2024-10-13 14:54:46 -0400

    BIG update!!
    very long overdue update: fixed fizzbuzz 2 code along with past ones

---
+++ scripts/scriptfizz4.js
@@ -0,0 +1,47 @@
+function fizzBuzzNew(e) {
+    const firstName = document.getElementById("firstname").value;
+    const lastName = document.getElementById("lastname").value;
+    const middleInitial = document.getElementById("middleinitial").value;
+
+    let firstDivisor = 3;
+    let secondDivisor = 5;
+
+    if (!firstName || !lastName ){
+      alert("Please enter your first and last name.");
+      return;
+    }
+
+    document.getElementById("greeting").innerText = "Welcome to Cy\'s Computers, " + firstName + " " + middleInitial + " " + lastName + "!";
+    e.preventDefault();
+
+
+    const fizzBuzzList2 = document.getElementById("fizzbuzzlist2");
+    document.getElementById("fizzbuzzlist2").innerText = "";
+
+function checkDivision (numberA, numberB) { //this function checks if the number can be divisible by certain numbers
+    if (numberA % numberB === 0) {
+    return true;
+  }
+}
+
+  for (let i = 0; i < 140; i++) {
+    let newFizzBuzzList2 = document.createElement("li");
+    let textContent = ""; //initializes textContent to add results of each check
+
+    if (checkDivision((i+1), firstDivisor)) { //checks to see if counter is divisible by firstDivisor (3)
+        textContent += "Error, retrying... "; 
+    }
+    if (checkDivision((i+1), secondDivisor)) { //checks to see if counter is divisible by secondDivisor (5)
+        textContent += "Connection interrupted... ";
+    }
+     if (!checkDivision((i + 1), firstDivisor) && !checkDivision((i + 1), secondDivisor)) {
+        textContent += "Computing..."; //if none match, this will be the generic output
+    }
+
+    newFizzBuzzList2.textContent = textContent;
+    fizzBuzzList2.appendChild(newFizzBuzzList2);
+}
+
+}
+
+fizzbutton2.addEventListener("click", fizzBuzzNew);
