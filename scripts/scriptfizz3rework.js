51fede11731c1fd272a165c9897edca1eb631f75
Christina Mekheil <mekheilc@gmail.com>
2024-11-21 21:27:37 -0500

    DI changes to fizz buzz 1
    - added reset button to fizz buzz 1 and beyond
    - added . to middle initial
    - shortened input field for middle initial (and other two)
    - removed the placeholders (rip law)

--- scripts/scriptfizz3rework.js
+++ scripts/scriptfizz3rework.js
@@ -1,12 +1,17 @@
 function fizzBuzz2(e) {
-    const firstName = document.getElementById("firstname").value;
-    const lastName = document.getElementById("lastname").value;
-    const middleInitial = document.getElementById("middleinitial").value;
+    const firstName = document.getElementById("firstname").value.trim();
+    const lastName = document.getElementById("lastname").value.trim();
+    let middleInitial = document.getElementById("middleinitial").value.trim();
+
+    if (middleInitial && !middleInitial.endsWith(".")) {
+        middleInitial += ".";
+    }
 
     if (!firstName || !lastName ){
       alert("Please enter your first and last name.");
       return;
     }
+      
 
     document.getElementById("greeting").innerText = "Welcome to Cy\'s Computers, " + firstName + " " + middleInitial + " " + lastName + "!";
     e.preventDefault();
@@ -15,22 +20,30 @@ function fizzBuzz2(e) {
     const fizzBuzzList2 = document.getElementById("fizzbuzzlist2");
     document.getElementById("fizzbuzzlist2").innerText = "";
 
-   for (let i = 0; i < 140; i++){
-        let newFizzBuzzList2 = document.createElement("li");
-        if ((i+1) % 5 === 0 && (i+1) % 3 === 0) { //checks to see if counter is divisible by 3 AND 5
-            newFizzBuzzList2.textContent = (i+1) + ") Connection interupted... " + "Error, retrying...";
-          fizzBuzzList2.appendChild(newFizzBuzzList2);
-        } else if ((i+1) % 3 === 0) { //checks to see if counter is divisible by 3 
-          newFizzBuzzList2.textContent = (i+1) + ") Error, retrying...";
-          fizzBuzzList2.appendChild(newFizzBuzzList2);
-        } else if ((i+1) % 5 === 0) { //checks to see if counter is divisible by 5
-            newFizzBuzzList2.textContent = (i+1) + ") Connection interupted...";
-          fizzBuzzList2.appendChild(newFizzBuzzList2);
-        }     else  { //if none match, this will be the generic output
-          newFizzBuzzList2.textContent = (i+1) + ") Computing...";
-          fizzBuzzList2.appendChild(newFizzBuzzList2);
-        }
-   }
+  function checkDivision (numberA, numberB) { //this function checks if the number can be divisible by certain numbers
+    if (numberA % numberB === 0) {
+    return true;
+  }
+}
+
+  for (let i = 0; i < 140; i++) {
+    let newFizzBuzzList2 = document.createElement("li");
+    let textContent = ""; //initializes textContent to add results of each check
+
+    if (checkDivision((i+1), firstDivisor)) { //checks to see if counter is divisible by firstDivisor (3)
+        textContent += "Error, retrying... "; 
+    }
+    if (checkDivision((i+1), secondDivisor)) { //checks to see if counter is divisible by secondDivisor (5)
+        textContent += "Connection interrupted... ";
+    }
+    if (!checkDivision((i + 1), firstDivisor) && !checkDivision((i + 1), secondDivisor)) {
+        textContent += "Computing..."; //if none match, this will be the generic output
+    }
+
+    newFizzBuzzList2.textContent = textContent;
+    fizzBuzzList2.appendChild(newFizzBuzzList2);
+}
+
 }
 
 fizzbutton2.addEventListener("click", fizzBuzz2);
--- scripts/scriptfizz4.js
+++ scripts/scriptfizz4.js
@@ -1,10 +1,14 @@
 function fizzBuzzNew(e) {
-    const firstName = document.getElementById("firstname").value;
-    const lastName = document.getElementById("lastname").value;
-    const middleInitial = document.getElementById("middleinitial").value;
+    const firstName = document.getElementById("firstname").value.trim();
+    const lastName = document.getElementById("lastname").value.trim();
+    let middleInitial = document.getElementById("middleinitial").value.trim();
 
     let firstDivisor = 3;
     let secondDivisor = 5;
+
+    if (middleInitial && !middleInitial.endsWith(".")) {
+        middleInitial += ".";
+    }
 
     if (!firstName || !lastName ){
       alert("Please enter your first and last name.");
@@ -34,7 +38,7 @@ function checkDivision (numberA, numberB) { //this function checks if the number
     if (checkDivision((i+1), secondDivisor)) { //checks to see if counter is divisible by secondDivisor (5)
         textContent += "Connection interrupted... ";
     }
-     if (!checkDivision((i + 1), firstDivisor) && !checkDivision((i + 1), secondDivisor)) {
+    if (!checkDivision((i + 1), firstDivisor) && !checkDivision((i + 1), secondDivisor)) {
         textContent += "Computing..."; //if none match, this will be the generic output
     }
 
